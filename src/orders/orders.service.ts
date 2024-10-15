043264ea67d0d9c5f0a9c4ae676fd7a8d0d7b9f0
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-10-15 19:39:39 -0300

    Adds nats service

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -9,16 +9,14 @@ import { CreateOrderDto } from './dto/create-order.dto';
 import { PrismaClient } from '@prisma/client';
 import { ChangeOrderStatusDto, OrderPaginationDto } from './dto';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
-import { PRODUCT_SERVICE } from 'src/config';
+import { NATS_SERVICE } from 'src/config';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger('Orders MS');
 
-  constructor(
-    @Inject(PRODUCT_SERVICE) private readonly productClient: ClientProxy,
-  ) {
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {
     super();
   }
 
@@ -161,7 +159,7 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
   async getProductsByIds(ids: number[]) {
     try {
       const products = await firstValueFrom(
-        this.productClient.send({ cmd: 'validate_products' }, ids),
+        this.client.send({ cmd: 'validate_products' }, ids),
       );
 
       return products;
