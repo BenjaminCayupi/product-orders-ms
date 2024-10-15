8bf81ef67ca3b9f360814ee8db95f4e7b7dbcdae
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-10-15 12:35:14 -0300

    Adds product service

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,11 +3,15 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+  PRODUCT_MICROSERVICE_HOST: string;
+  PRODUCT_MICROSERVICE_PORT: number;
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
+    PRODUCT_MICROSERVICE_HOST: joi.string().required(),
+    PRODUCT_MICROSERVICE_PORT: joi.number().required(),
   })
   .unknown(true);
 
@@ -21,4 +25,6 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
+  productMicroserviceHost: envVars.PRODUCT_MICROSERVICE_HOST,
+  productMicroservicePort: envVars.PRODUCT_MICROSERVICE_PORT,
 };
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { envs, PRODUCT_SERVICE } from 'src/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   controllers: [OrdersController],
   providers: [OrdersService],
+  imports: [
+    ClientsModule.register([
+      {
+        name: PRODUCT_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          host: envs.productMicroserviceHost,
+          port: envs.productMicroservicePort,
+        },
+      },
+    ]),
+  ],
 })
 export class OrdersModule {}
