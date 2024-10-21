0252158126338d8d4e1621b8bf83c5f4ddd2cb99
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-10-21 16:29:06 -0300

    Adds paid order dto

--- src/orders/dto/index.ts
+++ src/orders/dto/index.ts
@@ -3,3 +3,4 @@ export * from './update-order.dto';
 export * from './order-pagination.dto';
 export * from './change-order-status.dto';
 export * from './order-item.dto';
+export * from './paid-order.dto';
---
+++ src/orders/dto/paid-order.dto.ts
@@ -0,0 +1,13 @@
+import { IsString, IsUrl, IsUUID } from 'class-validator';
+
+export class PaidOrderDto {
+  @IsString()
+  stripePaymentId: string;
+
+  @IsString()
+  @IsUUID()
+  orderId: string;
+
+  @IsUrl()
+  receiptUrl: string;
+}
